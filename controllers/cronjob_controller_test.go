407a29158e8a2886fccdee49c359b2c5c2e013dd
shriacquia <shrinidhi.jahagirdar@acquia.com>
2023-04-14 20:36:26 +0530

    KB-1.10.0: First pass at validation

--- controllers/cronjob_controller_test.go
+++ controllers/cronjob_controller_test.go
@@ -9,8 +9,18 @@ import (
 	batchv1 "k8s.io/api/batch/v1"
 	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
 	cronjobv1 "tutorial.kubebuilder.io/project/api/v1"
 )
+
+/*
+The first step to writing a simple integration test is to actuall create an instance of CronJob you
+can run tests agains. Note that to create a CronJob, you'll need to create a stub CronJob struct
+that contains your CronJob's specifications.
+Note that when we create a stub CronJob, the CronJob also needs stubs required of its downstream
+objects. Without the stubbed JobTemplateSpec and the PodTemplateSpec below, the kube API will not
+be able to create the CronJob
+*/
 
 var _ = Describe("CronJob controller", func() {
 	// Define utility constants for object names and testing timeouts/durations and intervals
@@ -58,6 +68,30 @@ var _ = Describe("CronJob controller", func() {
 				},
 			}
 			Expect(k8sClient.Create(ctx, cronJob)).Should(Succeed())
+
+			/*
+				After creating this CronJob, let's check that the CronJob's Spec fields match what we passed in.
+				Note that, because the k8s apiserver may not have finished creating a CronJob after our
+				Create() call from earlier, we will use Gomega's `Eventually()` testing function instead of
+				`Expect()` to give the apiserver an opportunity to finish creating our CronJob.
+				`Eventually()` will repeatedly run the function provided as an argument every interval seconds
+				until
+					(a) the function's output matches what's expected in the subsequent `Should()` call or
+					(b) the number of attempts * interval period exceed the provided timeout value.
+
+				In the examples below, timeout and interval are Go Duration values of our choosing.
+			*/
+
+			cronjobLookupKey := types.NamespacedName{Name: CronjobName, Namespace: CronJobNamespace}
+			createdCronjob := &cronjobv1.CronJob{}
+
+			// We will have to retry getting this newly created CronJob, given that creation may not happen immediately.
+			Eventually(func() bool {
+				err := k8sClient.Get(ctx, cronjobLookupKey, createdCronjob)
+				return err == nil
+			}, timeout, interval).Should(BeTrue())
+			// Let's make sure our Schedule string value was properly converted/handled.
+			Expect(createdCronjob.Spec.Schedule).Should(Equal("1 * * * *"))
 
 		})
 	})
